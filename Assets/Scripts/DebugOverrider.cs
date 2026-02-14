bc0f75af80dba4df7b47ca906603c97baac12707
Alex-BoopyLoops-Christian <starttoast@gmail.com>
2026-02-14 17:33:47 -0500

    Unbroke a script

--- Assets/Scripts/DebugOverrider.cs
+++ Assets/Scripts/DebugOverrider.cs
@@ -1,12 +1,13 @@
 using UnityEngine;
+using System;
 using GameData;
 
-public class DebugOverrider : MonoBehaviour
-{
-    [SerializeField] public Agent agent;
+public class DebugOverrider : MonoBehaviour {
+    /*[SerializeField] public Agent agent;
     private List<Ability> Abilities = agent.GetAbilities();
-    
+
     public void UseAbility(int i) {
         // agent.UseAbility(Abilities[i],TARGETTILE);
-    }
+    }*/
+}
     
